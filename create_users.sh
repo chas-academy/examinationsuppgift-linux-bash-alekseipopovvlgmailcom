ebd06ea402122aeb2cf4dacadf9cca63d02a89b1
alekseipopovvlgmailcom <aleksei.popov.vl@gmail.com>
2026-05-16 20:55:57 +0200

    Update create_users.sh
    check root

--- create_users.sh
+++ create_users.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+if [ $UID -gt 0 ]; then
+    echo "Error: User is not eligible to run the script"
+    exit 1
+fi
+
+if [ $# -eq 0 ]; then
+    echo "No users provided"
+    exit 1
+fi
