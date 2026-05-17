d5747fce40649d43e1b9396203805c9d15cfded0
alekseipopovvlgmailcom <aleksei.popov.vl@gmail.com>
2026-05-17 20:19:28 +0200

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# check if the user is root
+#check if the user is root
 if [ $UID -gt 0 ]; then
-    echo "Error: User is not eligible to run this script!"
+    echo "Error: User is not eligible to run the script"
     exit 1
 fi
 
@@ -11,28 +11,28 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-
+# takes username and creates necessary folders for the user
 create_folders() {
     folders=(Documents Downloads Work)
 
     for folder in "${folders[@]}"; do
         path="/home/$1/$folder"
         mkdir "$path"
-        chmod "700" "$path"
+        chmod 700 "$path"
     done
+
     echo "Folders created"
 }
 
-
+# takes username and creates welcome file with corresponding header
 create_welcome_file() {
     path="/home/$1/welcome.txt"
     touch "$path"
-    echo "Välkommen $user" > "$path"
+    echo "Välkommen $1" > "$path"
 }
 
-
 for user in "$@"; do
-    if id "$user" &>/dev/null ; then
+    if id "$user" &>/dev/null; then
         echo "User: $user exists."
         continue
     fi
@@ -40,13 +40,12 @@ for user in "$@"; do
     echo "Creating user: $user"
 
     home_dir="/home/$user"
-
-    # create user with users directory
+    #crete user with corrresponding directory
     useradd -m "$user"
     create_folders "$user"
     create_welcome_file "$user"
 
-    # set ownership
+    #set ownership
     chown -R "$user:$user" "$home_dir"
 done
 
